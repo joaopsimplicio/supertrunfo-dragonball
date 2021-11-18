7f269ca6b0e82a72a44da208bd0cb165c21a42fa
joaopsimplicio <86742158+joaopsimplicio@users.noreply.github.com>
2021-11-17 21:09:58 -0300

    Adicionando arquivos do projeto
    Adicionando os arquivos da página principal do jogo Super Trunfo Dragon Ball Super

---
+++ script/script.js
@@ -0,0 +1,503 @@
+var carta1 = {
+    nome: "Son Goku",
+    imagem: "https://i.ytimg.com/vi/ZfMRJfMcfzk/hqdefault.jpg",
+    atributos: {
+        KI: 8700,
+        Técnicas: 8500,
+        Velocidade: 8500
+    }
+};
+
+var carta2 = {
+    nome: "Vegeta",
+    imagem:
+        "https://nerdhits.com.br/wp-content/uploads/2020/11/vegeta-dragon-ball-z-1200x900.jpg",
+    atributos: {
+        KI: 8500,
+        Técnicas: 8200,
+        Velocidade: 8500
+    }
+};
+
+var carta3 = {
+    nome: "Broly",
+    imagem: "https://img.haikudeck.com/r/87f52428-0062-416f-9e0.jpg",
+    atributos: {
+        KI: 9000,
+        Técnicas: 7500,
+        Velocidade: 7800
+    }
+};
+
+var carta4 = {
+    nome: "Gohan",
+    imagem: "https://conteudo.imguol.com.br/c/entretenimento/9c/2017/07/14/gohan-1500057808905_v2_450x337.jpg",
+    atributos: {
+        KI: 7800,
+        Técnicas: 7100,
+        Velocidade: 8000
+    }
+};
+
+var carta5 = {
+    nome: "Trunks",
+    imagem: "https://www.torredevigilancia.com/wp-content/uploads/2016/06/Dragon-Ball-Super-Trunks-do-Futuro.jpg",
+    atributos: {
+        KI: 8100,
+        Técnicas: 8300,
+        Velocidade: 7700
+    }
+};
+
+var carta6 = {
+    nome: "Piccolo",
+    imagem: "https://nerdhits.com.br/wp-content/uploads/2021/07/piccolo-idade-dragon-ball-super-super-hero-1200x900.jpg",
+    atributos: {
+        KI: 7000,
+        Técnicas: 7200,
+        Velocidade: 6500
+    }
+};
+
+var carta7 = {
+    nome: "Freeza",
+    imagem: "https://www.einerd.com.br/wp-content/uploads/2018/04/freeza-dourado-890x501.jpg",
+    atributos: {
+        KI: 8200,
+        Técnicas: 8300,
+        Velocidade: 7700
+    }
+};
+
+var carta8 = {
+    nome: "Majin Boo",
+    imagem: "https://sm.ign.com/ign_br/screenshot/default/majin-boo-dragon-ball-z_3671.jpg",
+    atributos: {
+        KI: 8300,
+        Técnicas: 9000,
+        Velocidade: 7200
+    }
+};
+
+var carta9 = {
+    nome: "Cell",
+    imagem: "https://criticalhits.com.br/wp-content/uploads/2021/07/Dragon-Ball-Super-CELL.jpg",
+    atributos: {
+        KI: 7400,
+        Técnicas: 6000,
+        Velocidade: 6600
+    }
+};
+
+var carta10 = {
+    nome: "Goku Black",
+    imagem: "https://criticalhits.com.br/wp-content/uploads/2019/03/goku-black-01-910x607.jpg",
+    atributos: {
+        KI: 9000,
+        Técnicas: 8600,
+        Velocidade: 8100
+    }
+};
+
+var carta11 = {
+    nome: "Zamasu",
+    imagem: "https://besthqwallpapers.com/Uploads/6-7-2018/58495/thumb2-4k-zamasu-smoke-dragon-ball-super-artwork.jpg",
+    atributos: {
+        KI: 9400,
+        Técnicas: 8600,
+        Velocidade: 8400
+    }
+};
+
+var carta12 = {
+    nome: "Hit",
+    imagem: "https://www.comboinfinito.com.br/principal/wp-content/uploads/2017/12/Hit-dragon-ball-super.jpg",
+    atributos: {
+        KI: 8200,
+        Técnicas: 8800,
+        Velocidade: 9500
+    }
+};
+
+var carta13 = {
+    nome: "Gogeta",
+    imagem: "https://i.ytimg.com/vi/pwmpBxDmhJM/sddefault.jpg",
+    atributos: {
+        KI: 9500,
+        Técnicas: 9600,
+        Velocidade: 8800
+    }
+};
+
+var carta14 = {
+    nome: "Vegetto",
+    imagem: "https://criticalhits.com.br/wp-content/uploads/2020/03/vegeto.jpg",
+    atributos: {
+        KI: 9200,
+        Técnicas: 9500,
+        Velocidade: 9000
+    }
+};
+
+var carta15 = {
+    nome: "Gotenks",
+    imagem: "https://criticalhits.com.br/wp-content/uploads/2018/04/gotenks-ssj3-01.jpg",
+    atributos: {
+        KI: 8000,
+        Técnicas: 8500,
+        Velocidade: 7200
+    }
+};
+
+var carta16 = {
+    nome: "Mestre Kame",
+    imagem: "https://i.pinimg.com/474x/19/fc/98/19fc9847412c8085f62e5902724aa42b.jpg",
+    atributos: {
+        KI: 4000,
+        Técnicas: 9100,
+        Velocidade: 5800
+    }
+};
+
+var carta17 = {
+    nome: "Kuririn",
+    imagem: "https://criticalhits.com.br/wp-content/uploads/2020/04/kuririn.jpg",
+    atributos: {
+        KI: 5500,
+        Técnicas: 7200,
+        Velocidade: 6800
+    }
+};
+
+var carta18 = {
+    nome: "Shenlong",
+    imagem: "https://img.elo7.com.br/product/zoom/29C03B9/adesivo-buraco-dragon-bal-z-shenlong-decorativo-sua-escolha-adesivo-parede-quarto.jpg",
+    atributos: {
+        KI: 1000000,
+        Técnicas: 1000000,
+        Velocidade: 1000000
+    }
+};
+
+var carta19 = {
+    nome: "Jiren",
+    imagem: "https://i.pinimg.com/736x/bd/4f/62/bd4f62cb83476eb07eff50256518c3e4.jpg",
+    atributos: {
+        KI: 10000,
+        Técnicas: 9000,
+        Velocidade: 9200
+    }
+};
+
+var carta20 = {
+    nome: "Toppo",
+    imagem: "https://besthqwallpapers.com/Uploads/31-1-2018/38915/thumb2-toppo-dragon-4k-dragon-ball-art-dbs.jpg",
+    atributos: {
+        KI: 9200,
+        Técnicas: 8100,
+        Velocidade: 7800
+    }
+};
+
+var carta21 = {
+    nome: "Dispo",
+    imagem: "https://i.redd.it/gu079xxzqyh71.jpg",
+    atributos: {
+        KI: 8000,
+        Técnicas: 7900,
+        Velocidade: 10000
+    }
+};
+
+var carta22 = {
+    nome: "Aniraza",
+    imagem: "https://criticalhits.com.br/wp-content/uploads/2017/12/bb4374a8ad72a2d0f95f52b52f361ce5.jpg",
+    atributos: {
+        KI: 9000,
+        Técnicas: 7000,
+        Velocidade: 7500
+    }
+};
+
+var carta23 = {
+    nome: "Saonel",
+    imagem: "http://pm1.narvii.com/6681/4a12075c4c19d48f456d22a8ea09da5d45c47021_00.jpg",
+    atributos: {
+        KI: 9000,
+        Técnicas: 7500,
+        Velocidade: 7800
+    }
+};
+
+var carta24 = {
+    nome: "Pilina",
+    imagem: "https://i.pinimg.com/originals/c5/bb/3a/c5bb3ab3a281e635aed1ae74e5f1bc6e.jpg",
+    atributos: {
+        KI: 7500,
+        Técnicas: 7000,
+        Velocidade: 6400
+    }
+};
+
+var carta25 = {
+    nome: "Kefla",
+    imagem: "https://i.pinimg.com/originals/07/93/3b/07933b9c8d28901fc3ee52961841f251.jpg",
+    atributos: {
+        KI: 9200,
+        Técnicas: 8400,
+        Velocidade: 8400
+    }
+};
+
+var carta26 = {
+    nome: "Caulifla",
+    imagem: "https://sm.ign.com/ign_br/screenshot/default/dragon-ball-caulifla_z8j2.jpg",
+    atributos: {
+        KI: 8100,
+        Técnicas: 8000,
+        Velocidade: 7500
+    }
+};
+
+var carta27 = {
+    nome: "Kale",
+    imagem: "https://i.pinimg.com/1200x/f3/90/7c/f3907c67faafbe038364a39849808260.jpg",
+    atributos: {
+        KI: 8500,
+        Técnicas: 7500,
+        Velocidade: 7400
+    }
+};
+
+var carta28 = {
+    nome: "Kyabe",
+    imagem: "https://nerdhits.com.br/wp-content/uploads/2021/07/kyabe-dragon-ball-z-1200x720.jpg",
+    atributos: {
+        KI: 8000,
+        Técnicas: 8000,
+        Velocidade: 7700
+    }
+};
+
+var carta29 = {
+    nome: "Frost",
+    imagem: "https://images.alphacoders.com/922/thumb-1920-922322.jpg",
+    atributos: {
+        KI: 7100,
+        Técnicas: 6000,
+        Velocidade: 7000
+    }
+};
+
+var carta30 = {
+    nome: "Ribrianne",
+    imagem: "https://puloduplo.com.br/wp-content/uploads/2019/05/DBXV2-Ribrianne_05-16-19.jpg",
+    atributos: {
+        KI: 6700,
+        Técnicas: 7000,
+        Velocidade: 7100
+    }
+};
+
+var carta31 = {
+    nome: "Bardock",
+    imagem: "https://criticalhits.com.br/wp-content/uploads/2021/09/Bardock-Dragon-Ball-capa-1.jpg",
+    atributos: {
+        KI: 8000,
+        Técnicas: 7000,
+        Velocidade: 8100
+    }
+};
+
+var carta32 = {
+    nome: "Tenshinhan",
+    imagem: "https://criticalhits.com.br/wp-content/uploads/2020/06/Tenshinhan-910x510.jpg",
+    atributos: {
+        KI: 6500,
+        Técnicas: 6400,
+        Velocidade: 6600
+    }
+};
+
+var carta33 = {
+    nome: "Android 17",
+    imagem: "https://ovicio.com.br/wp-content/uploads/2020/08/20200820-dragon-ball_-androide-17-saca-su-lado-mas-femenino-con-este-cosplay.jpg",
+    atributos: {
+        KI: 7300,
+        Técnicas: 7100,
+        Velocidade: 8400
+    }
+};
+
+var carta34 = {
+    nome: "Android 18",
+    imagem: "https://i0.wp.com/media.comicbook.com/2017/11/dragon-ball-super-18-1060945-1280x0.jpg",
+    atributos: {
+        KI: 7100,
+        Técnicas: 6700,
+        Velocidade: 8300
+    }
+};
+
+var carta35 = {
+    nome: "Magetta",
+    imagem: "https://m.media-amazon.com/images/M/MV5BYmZhYTBmNGQtNzhmMy00ZjEyLWI1Y2UtOWYzN2UwMWRjN2VlXkEyXkFqcGdeQXVyMTk3OTMzOA@@._V1_.jpg",
+    atributos: {
+        KI: 6600,
+        Técnicas: 5500,
+        Velocidade: 5000
+    }
+};
+
+var carta36 = {
+    nome: "Botamo",
+    imagem: "https://geekireland.com/wp-content/uploads/2016/02/Dragon-Ball-Super-Botamo.png",
+    atributos: {
+        KI: 5000,
+        Técnicas: 5700,
+        Velocidade: 5500
+    }
+};
+
+var cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10,
+    carta11, carta12, carta13, carta14, carta15, carta16, carta17, carta18, carta19, carta20,
+    carta21, carta22, carta23, carta24, carta25, carta26, carta27, carta28, carta29, carta30,
+    carta31, carta32, carta33, carta34, carta35, carta36];
+var cartaMaquina;
+var cartaJogador;
+
+var pontosJogador = 0
+var pontosMaquina = 0
+atualizaPlacar()
+atualizaQuantidadeCartas()
+
+function atualizaQuantidadeCartas() {
+    var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+    var html = "Cartas restantes: " + cartas.length
+
+    divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar() {
+    var divPlacar = document.getElementById('placar')
+    var html = "Você " + pontosJogador + " / " + pontosMaquina + " Computador"
+    divPlacar.innerHTML = html
+}
+
+function exibirApenasMolduraCartaMaquina() {
+
+    var molduraCartaMaquina = document.getElementById("carta-maquina");
+
+    molduraCartaMaquina.innerHTML = "<img src='https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png' style=' width: inherit; height: inherit; position: absolute;'>"
+
+    molduraCartaMaquina.style.backgroundImage = "";
+
+    var divResultado = document.getElementById("resultado")
+    divResultado.innerHTML = " "
+
+}
+
+function sortearCarta() {
+
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+    cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina, 1)
+
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    cartaJogador = cartas[numeroCartaJogador]
+    cartas.splice(numeroCartaJogador, 1)
+
+    document.getElementById('esolher-atributo')
+
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+
+    exibirApenasMolduraCartaMaquina();
+    exibeCartaJogador();
+}
+
+function exibeCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">'
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto +=
+            "<label for='id_" + atributo + "'>" +
+            "<input type='radio' id='id_" + atributo + "' name='atributo' value='" + atributo + "'>" +
+            atributo + " " + cartaJogador.atributos[atributo] +
+            "</label>" +
+            "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status'>"
+
+    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Venceu!</p>'
+        pontosJogador++
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Perdeu...</p>'
+        pontosMaquina++
+    } else {
+        htmlResultado = '<p class="resultado-final">Empatou</p>'
+    }
+
+    if (cartas.length == 0) {
+        document.getElementById("btnSortear").disabled = true;
+        if (pontosJogador > pontosMaquina) {
+            htmlResultado = '<p class="resultado-final">Você venceu! Parabéns!</p>'
+        } else if (pontosMaquina > pontosJogador) {
+            htmlResultado = '<p class="resultado-final">Você perdeu... Tente novamente</p>'
+        } else {
+            htmlResultado = '<p class="resultado-final">Empatou</p>'
+        }
+    } else {
+        document.getElementById('btnSortear').disabled = false
+    }
+
+    divResultado.innerHTML = htmlResultado
+
+    document.getElementById('btnSortear').disabled = false
+    document.getElementById('btnJogar').disabled = true
+
+    exibeCartaMaquina()
+    atualizaPlacar()
+    atualizaQuantidadeCartas()
+}
+
+function exibeCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">'
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaMaquina.atributos) {
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status'>"
+
+    divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function refreshPage() {
+    window.location.reload();
+}
